bcbbc6297c6610926a92b05c013436981f605070
ilhamdwikarifani <ilhamdwiki0408@gmail.com>
2023-10-17 22:07:27 +0700

    add image and Level

--- personal.js
+++ personal.js
@@ -1,5 +1,8 @@
 const questions = [
   {
+    level: "1",
+    image:
+      "https://www.ebookanak.com/wp-content/uploads/Gambar-47-Membersihkan-dan-Merapikan-Tempat-Tidur.jpg",
     question: "Apa yang dilakukan setelah bangun tidur?",
     choice1: "Pergi",
     choice2: "Merapikan Tempat Tidur",
@@ -9,6 +12,9 @@ const questions = [
       "Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.",
   },
   {
+    level: "2",
+    image:
+      "https://janethes.com/wp-content/uploads/2019/01/mengajarkan-rutinitas-gosok-gigi-pada-anak_Artboard-2.png",
     question: "Apa yang dilakukan untuk menjaga gigi tetap sehat?",
     choice1: "menyikat gigi",
     choice2: "menyikat kaki",
@@ -18,6 +24,9 @@ const questions = [
       "Menyikat gigi merupakan rutinitas yang dilakukan untuk memelihara kebersihan dan kesehatan gigi dan mulut",
   },
   {
+    level: "3",
+    image:
+      "https://www.ebookanak.com/wp-content/uploads/Gambar-48-Keramas-dengan-Sampo-Saat-Mandi.jpg",
     question: ": Apa yang dilakukan untuk membersihkan badan?",
     choice1: "Mandi",
     choice2: "Makan",
@@ -31,6 +40,9 @@ const questions = [
 let currentQuestion = 0;
 let skor = 1;
 
+const levelElement = document.getElementById("level");
+const levelmodalElement = document.getElementById("levelModal");
+const imageElement = document.getElementById("image");
 const questionElement = document.getElementById("question");
 const choice1Element = document.getElementById("choice1");
 const choice2Element = document.getElementById("choice2");
@@ -40,6 +52,9 @@ const contentElement = document.getElementById("content");
 const skorElement = document.getElementById("skor");
 
 function displayQuestion() {
+  levelElement.textContent = questions[currentQuestion].level;
+  levelmodalElement.textContent = questions[currentQuestion].level;
+  imageElement.src = questions[currentQuestion].image;
   questionElement.textContent = questions[currentQuestion].question;
   choice1Element.textContent = questions[currentQuestion].choice1;
   choice2Element.textContent = questions[currentQuestion].choice2;
@@ -49,6 +64,8 @@ function displayQuestion() {
 }
 
 displayQuestion();
+
+console.log(questions[currentQuestion]);
 
 function checkAnswer(answer) {
   if (answer === questions[currentQuestion].answer) {
@@ -94,4 +111,3 @@ function endModal() {
   document.getElementById("end-modal").style.display = "none";
   window.location.reload();
 }
- 
