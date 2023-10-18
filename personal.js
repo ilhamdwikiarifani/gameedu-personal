2e50e72dfa6a81e3efb814f93486af774e296e8d
ilhamdwikarifani <ilhamdwiki0408@gmail.com>
2023-10-18 20:47:37 +0700

    update

--- personal.js
+++ personal.js
@@ -8,7 +8,7 @@ const questions = [
     answer: "Merapikan Tempat Tidur",
     judul: "TIPS MERAPIKAN TEMPAT TIDUR",
     content:
-      "Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat. Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.",
+      "Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat. Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap pagi adalah hal sederhana namun merupakan kebiasaan yang bermanfaat.Merapikan tempat tidur setiap ",
   },
   {
     level: "2",
@@ -33,6 +33,90 @@ const questions = [
     judul: "TIPS MANDI YANG BAIK",
     content:
       "Mandi adalah kegiatan perawatan kulit harian yang memberikan banyak manfaat. Namun, agar mandi benar dan efektif, ada beberapa tips yang perlu diperhatikan yang dijelaskan berikut in",
+  },
+  {
+    level: "4",
+    image:
+      "https://2.bp.blogspot.com/-YIoowFnf0sA/V1z6FM9NHII/AAAAAAAAAG4/0MC2l18pYNIOwuZwlOJxiO0scVaFDBX1wCLcB/s1600/hqdefault.jpg",
+    question: "Apa yang dilakukan untuk menutupi anggota badan?",
+    choice1: "Memakai Baju",
+    choice2: "Makan",
+    answer: "Memakai Baju",
+    judul: "TIPS MEMAKAI BAJU",
+    content:
+      "Mandi adalah kegiatan perawatan kulit harian yang memberikan banyak manfaat. Namun, agar mandi benar dan efektif, ada beberapa tips yang perlu diperhatikan yang dijelaskan berikut in",
+  },
+  {
+    level: "5",
+    image:
+      "https://core-ruangguru.s3.amazonaws.com/assets/ruang_belajar/questions/q_5pyfrl9628.png",
+    question:
+      "Apa yang dilakukan untuk lantai tetap bersih dari debu dan kotoran",
+    choice1: "Mandi",
+    choice2: "Menyapu dan Mengepel",
+    answer: "Menyapu dan Mengepel",
+    judul: "TIPS MENYAPU DAN MENGEPEL",
+    content:
+      "Mandi adalah kegiatan perawatan kulit harian yang memberikan banyak manfaat. Namun, agar mandi benar dan efektif, ada beberapa tips yang perlu diperhatikan yang dijelaskan berikut in",
+  },
+  {
+    level: "6",
+    image:
+      "https://asset-a.grid.id//crop/0x0:0x0/360x240/photo/2019/02/08/1246883665.jpg",
+    question: "Apa yang dilakukan untuk mendukung aktivitas di pagi hari?",
+    choice1: "Sarapan",
+    choice2: "Makan",
+    answer: "Sarapan",
+    judul: "TIPS MAKAN SEHAT",
+    content:
+      "Mandi adalah kegiatan perawatan kulit harian yang memberikan banyak manfaat. Namun, agar mandi benar dan efektif, ada beberapa tips yang perlu diperhatikan yang dijelaskan berikut in",
+  },
+  {
+    level: "7",
+    image:
+      "https://bimamedia-gurusiana.ap-south-1.linodeobjects.com/d7aacb93a8760f4df821d95bf117ec4b/2021/05/01/l-img20210501120007jpg20210501110058.jpeg",
+    question: " apa yang dilakukan untuk setelah sarapan?",
+    choice1: "Mandi",
+    choice2: "Mencuci piring",
+    answer: "Mencuci piring",
+    judul: "TIPS MENCUCI PIRING",
+    content:
+      "Mandi adalah kegiatan perawatan kulit harian yang memberikan banyak manfaat. Namun, agar mandi benar dan efektif, ada beberapa tips yang perlu diperhatikan yang dijelaskan berikut in",
+  },
+  {
+    level: "8",
+    image:
+      "https://legutykids.com/wp-content/uploads/2021/10/ilustrasi-kiat-jitu-untuk-anak-hebat.png",
+    question: "Apa yang dilakukan untuk membersihkan baju setelah dipakai?",
+    choice1: " Mencuci baju",
+    choice2: "Makan",
+    answer: " Mencuci baju",
+    judul: "TIPS MENCUCI BAJU",
+    content:
+      "Mandi adalah kegiatan perawatan kulit harian yang memberikan banyak manfaat. Namun, agar mandi benar dan efektif, ada beberapa tips yang perlu diperhatikan yang dijelaskan berikut in",
+  },
+  {
+    level: "9",
+    image: "https://cdn.pnghd.pics/data/473/gambar-orang-menjemur-baju-3.jpg",
+    question:
+      "apa manfaat dari angin dan matahari? Apa yang dilakukan agar pakaian kering?",
+    choice1: "Menjemur baju",
+    choice2: "Makan",
+    answer: "Menjemur baju",
+    judul: "TIPS MENJEMUR PAKAIAN",
+    content:
+      "Mengeringkan pakaian langsung di bawah sinar matahari merupakan cara tradisional yang sudah dilakukan sejak dulu. Meski relatif tradisional dan sudah ditemukan mesin pengering yang bisa mengeringkan pakaian lebih cepat dari tenaga surya, ada beberapa anfaat menjemur pakaian langsung dengan sinar matahari, salah satunya lebih murah dan emat energi. Umumnya, jemuran pakaian dapat kering dalam waktu 3–5 jam di bawah sinar matahari yang cerah.",
+  },
+  {
+    level: "10",
+    image: "https://roysayur.files.wordpress.com/2022/08/95d1b-ironing.jpg",
+    question: " apa yang dilakukan untuk menjaga kerapian baju",
+    choice1: "Mandi",
+    choice2: "Menyetrika",
+    answer: "Menyetrika",
+    judul: "TIPS MENYETRIKA",
+    content:
+      "Setrika sesuai fungsinya digunakan untuk melicinkan pakaian agar tidak lecek dan kusut. Selain itu, manfaat dari setrika yaitu membunuh kuman dan menghilangkan jamur. a. Untuk menyetrika pakaian dimulai dari bagian dalam pakaian terlebih dahulu, seperti kerah, lengan, dan bagian dalam baju. b. Kemudian, setrika bagian luar pakaian seperti bagian depan dan belakang baju. c. Jangan lupa untuk menyetrika bagian lengan baju dengan cara yang sama seperti menyetrika bagian depan dan belakang baju. d. Setelah selesai, biarkan pakaian dingin terlebih dahulu sebelum dilipat atau digantung",
   },
 ];
 
